7f091aaf655db91d37a0c4d0f14913c370ec91d8
chasemc67 <chasemc67@gmail.com>
2020-04-12 13:56:18 -0700

    inital commit

---
+++ search_bing_api.py
@@ -0,0 +1,80 @@
+from requests import exceptions
+import argparse
+import requests
+import cv2
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-q", '--query', required=True, help="Seach query for Bing Image API")
+ap.add_argument("-o", "--output", required=True, help="Path to output directory")
+args = vars(ap.parse_args())
+
+API_KEY = "API KEY HERE"
+MAX_RESULTS = 250
+GROUP_SIZE = 50
+
+URL= "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
+
+EXCEPTIONS = set([IOError, FileNotFoundError, exceptions.RequestException, exceptions.HTTPError, exceptions.ConnectionError, exceptions.Timeout])
+
+# store the search term in a convenience variable then set the
+# headers and search parameters
+term = args["query"]
+headers = {"Ocp-Apim-Subscription-Key" : API_KEY}
+params = {"q": term, "offset": 0, "count": GROUP_SIZE}
+
+# make the search
+print("[INFO] searching Bing API for '{}'".format(term))
+search = requests.get(URL, headers=headers, params=params)
+search.raise_for_status()
+
+# grab the results from the search, including the total number of
+# estimated results returned by the Bing API
+results = search.json()
+estNumResults = min(results["totalEstimatedMatches"], MAX_RESULTS)
+print("[INFO] {} total results for '{}'".format(estNumResults, term))
+
+# initialize the total number of images downloaded thus far
+total = 0
+
+# loop over the estimated number of results in `GROUP_SIZE` groups
+for offset in range(0, estNumResults, GROUP_SIZE):
+    # update the search parameters using the current offset, then
+    # make the request to fetch the results
+    print("[INFO] making request for group {}-{} of {}...".format(offset, offset + GROUP_SIZE, estNumResults))
+    params["offset"] = offset
+    search = requests.get(URL, headers=headers, params=params)
+    search.raise_for_status()
+    results = search.json()
+    print("[INFO] saving images for group {}-{} of {}...".format(offset, offset + GROUP_SIZE, estNumResults))
+
+    for v in results["value"]:
+        try:
+            # make request to download image
+            print("[INFO] fetching: {}".format(v["contentUrl"]))
+            r = requests.get(v["contentUrl"], timeout=30)
+
+            # build path to output image
+            ext = v["contentUrl"][v["contentUrl"].rfind("."):]
+            p = os.path.sep.join([args["output"], "{}{}".format(str(total).zfill(8), ext)])
+
+            # write image to disk
+            f = open(p, "wb")
+            f.write(r.content)
+            f.close()
+
+        except Exception as e:
+            if type(e) in EXCEPTIONS:
+                print("[INFO] skipping: {}".format(v["contentUrl"]))
+                continue
+
+        # try to load the image from disk to see if it worked
+        image = cv2.imread(p)
+
+        if image is None:
+            print("[INFO] deleting: {}".format(p))
+            os.remove(p)
+            continue
+
+        total += 1
+
